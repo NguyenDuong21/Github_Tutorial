0f97003a2f180643e6a29e38fee7f50fb065c876
NguyenDuong21 <thien123111999@gmail.com>
2020-06-29 16:11:48 +0700

    cac lenh git co ban

--- 10gitcoban.js
+++ 10gitcoban.js
@@ -1,3 +1,10 @@
 // git init: khởi tạo local repo trên folder bất kỳ
 // liên kết local repol với remote repo trên github: git remote add origin [Github_Repol_URL]
 // git add teenffile : đẩy file vào staging area 
+// git commit -m "descriptive message": lưu lại snapshots (ảnh chụp) của những thay đổi trong folder
+// git commit --amend -m "amend descriptive" : sửa lại đoạn mô tả
+// git push origin [branch-name] : Uploads tất cả các comit lên Github (tên nhành master)
+// git status : check trạng thái đang làm việc 
+// git diff : xem những thay đổi giữa các commit với nhau VD: git diff b325c da985
+// git log : hiển thị những commit . VD : 1. git log -<n>: hiển thị n commit, 2. git log --oneline : hiển thị toàn bộ commit
+// git reset tênfile : xóa file ra khỏi staging area
