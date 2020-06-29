d8b024f386eeaac58851379314ccd9e08e369cfb
NguyenDuong21 <thien123111999@gmail.com>
2020-06-29 09:36:25 +0700

    change file test.js

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
-var array = [1,2,3,4,5,6];
-console.log(array);
+function breakingRecords(scores) {
+    var curent1=scores[0],curent2=scores[0];
+    var dem1=0, dem2=0;
+    for (const element of scores) {
+        if(element>curent1) {
+            curent1=element;
+            dem1++;
+        } else if(element<curent2){
+            curent2=element;
+            dem2++;
+        }
+    }
+    console.log(dem1,dem2);
+}
+var arr=[10,5,20,20,4,5,2,25,1];
+breakingRecords(arr);
