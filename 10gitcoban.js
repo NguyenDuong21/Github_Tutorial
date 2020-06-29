050cc74c5f423586fe30c50459d025ff3f27754e
NguyenDuong21 <thien123111999@gmail.com>
2020-06-29 16:03:03 +0700

    10 lenh git co ban

---
+++ 10gitcoban.js
@@ -0,0 +1,3 @@
+// git init: khởi tạo local repo trên folder bất kỳ
+// liên kết local repol với remote repo trên github: git remote add origin [Github_Repol_URL]
+// git add teenffile : đẩy file vào staging area 
